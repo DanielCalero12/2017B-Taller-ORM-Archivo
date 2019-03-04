cd504ddc2f6f51e1b78503ea2c318346689e3395
DanielCalero12 <Gasvaroth@gmail.com>
2019-03-04 04:25:51 +0000

    Heroku first commit

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var indexRouter = require('./routes/index');
+var contacts = require('./routes/contact');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/contacts', contacts);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
