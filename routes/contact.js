674ee4a4e89e99382732abffd531c5185e72c572
DanielCalero12 <Gasvaroth@gmail.com>
2019-03-13 22:54:44 +0000

    indentando codigo

--- routes/contact.js
+++ routes/contact.js
@@ -3,11 +3,57 @@ var express = require('express');
 var Contact = require('../models/contact');
 var router = express.Router();
 router.route('/')
-.get(function(req, res) {
-Contact
-.fetchAll()
-.then(function(contacts) {
-res.json({ contacts });
+    .get(function(req, res) {
+        Contact
+            .fetchAll()
+            .then(function(contacts) {
+                res.json({
+                    contacts
+                });
+            });
+    })
+.post(function(req, res) {
+    new Contact({
+            id: req.body.id,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            emailAddress: req.body.emailAddress,
+        })
+        .save()
+        .then(function(saved) {
+            res.json({
+                saved
+            });
+        });
 });
+router.route('/:id')
+    .put(function(req, res) {
+        Contact
+            .where('id', req.params.id)
+            .fetch()
+            .then(function(contact) {
+                contact
+                    .save({
+                        id: req.body.id,
+                        firstName: req.body.firstName,
+                        lastName: req.body.lastName,
+                        emailAddress: req.body.emailAddress,
+                    })
+                    .then(function(saved) {
+                        res.json({
+                            saved
+                        });
+                    });
+            });
+    })
+.delete(function(req, res) {
+    Contact
+        .where('id', req.params.id)
+        .destroy()
+        .then(function(destroyed) {
+            res.json({
+                destroyed
+            });
+        });
 });
 module.exports = router;
